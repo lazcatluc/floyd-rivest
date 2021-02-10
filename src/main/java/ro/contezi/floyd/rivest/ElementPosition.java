2e3f86729ec8390db4b293ff3ad16581989e08e0
Catalin Lazar <catalin.lazar.end@nivaura.com>
2021-02-10 09:01:45 +0000

    Add position state to ElementPosition to prepare for multi-node computations

--- src/main/java/ro/contezi/floyd/rivest/ElementPosition.java
+++ src/main/java/ro/contezi/floyd/rivest/ElementPosition.java
@@ -1,12 +1,17 @@
 package ro.contezi.floyd.rivest;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Comparator;
+import java.util.List;
 
 public class ElementPosition<T> {
 
     private final Collection<T> elements;
     private final Comparator<? super T> comparator;
+    private List<T> lower;
+    private List<T> higherOrEqual;
+    private T positioned;
 
     private ElementPosition(Collection<T> elements, Comparator<? super T> comparator) {
         this.elements = elements;
@@ -17,11 +22,53 @@ public class ElementPosition<T> {
         return elements.parallelStream().filter(e -> comparator.compare(e, element) < 0).count();
     }
 
+    public ElementPosition<T> position(T element) {
+        if (positioned == null) {
+            lower = new ArrayList<>();
+            higherOrEqual = new ArrayList<>();
+            elements.forEach(e -> {
+                if (comparator.compare(e, element) < 0) {
+                    lower.add(e);
+                } else {
+                    higherOrEqual.add(e);
+                }
+            });
+        } else if (comparator.compare(element, positioned) < 0) {
+            List<T> newLower = new ArrayList<>();
+            higherOrEqual = new ArrayList<>();
+            lower.forEach(e -> {
+                if (comparator.compare(e, element) < 0) {
+                    newLower.add(e);
+                } else {
+                    higherOrEqual.add(e);
+                }
+            });
+            lower = newLower;
+        } else {
+            lower = new ArrayList<>();
+            List<T> newHigherOrEqual = new ArrayList<>();
+            higherOrEqual.forEach(e -> {
+                if (comparator.compare(e, element) < 0) {
+                    lower.add(e);
+                } else {
+                    newHigherOrEqual.add(e);
+                }
+            });
+            higherOrEqual = newHigherOrEqual;
+        }
+        positioned = element;
+        return this;
+    }
+
+    public int getPositionedRank() {
+        return lower.size();
+    }
+
     public static <T> ElementPosition<T> in(Collection<T> elements, Comparator<? super T> comparator) {
         return new ElementPosition<>(elements, comparator);
     }
  
     public static <T extends Comparable<? super T>> ElementPosition<T> in(Collection<T> elements) {
-        return new ElementPosition<>(elements, (e1, e2) -> e1.compareTo(e2));
+        return new ElementPosition<>(elements, Comparator.naturalOrder());
     }
 }
--- src/test/java/ro/contezi/floyd/rivest/ElementPositionTest.java
+++ src/test/java/ro/contezi/floyd/rivest/ElementPositionTest.java
@@ -8,10 +8,25 @@ import java.util.List;
 import org.junit.Test;
 
 public class ElementPositionTest {
+    private List<Integer> numbers = Arrays.asList(5, 0, 9, 10, 2, 6, 3, 4, 1, 8);
+
     @Test
-    public void finds4ToBeThe4thElementInNaturalNumbers() throws Exception {
-        List<Integer> numbers = Arrays.asList(5, 0, 9, 10, 2, 6, 3, 4, 1, 8);
-        
+    public void finds4ToBeThe5thElementInNaturalNumbers() {
         assertThat(ElementPosition.in(numbers).of(4)).isEqualTo(4);
+    }
+
+    @Test
+    public void positions4AsThe5thElementInNaturalNumbers() {
+        assertThat(ElementPosition.in(numbers).position(4).getPositionedRank()).isEqualTo(4);
+    }
+
+    @Test
+    public void positions6AsThe2ndElementInNaturalNumbersAfterHavingPositioned4() {
+        assertThat(ElementPosition.in(numbers).position(4).position(6).getPositionedRank()).isEqualTo(2);
+    }
+
+    @Test
+    public void positions5AsThe1stElementInNaturalNumbersAfterHavingPositioned4And6() {
+        assertThat(ElementPosition.in(numbers).position(4).position(6).position(5).getPositionedRank()).isEqualTo(1);
     }
 }
